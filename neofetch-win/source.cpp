dd27455d9d6931c5c9ba45d724c50bc27da655c7
nepnep <nepnep@hrzn.email>
2023-02-24 15:04:59 -0500

    redundancy fix

--- neofetch-win/source.cpp
+++ neofetch-win/source.cpp
@@ -26,7 +26,42 @@ std::wstring gethostname() {
 	return userhost;
 }
 
-std::wstring getwinver() {
+std::wstring getwinver1() {
+
+	std::wstring car_bomb;
+	using namespace m4x1m1l14n;
+
+	try
+	{
+		auto key = Registry::LocalMachine->Open(L"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion");
+
+		auto car_bomb = key->GetString(L"ProductName");
+
+		std::wstring s2 = L"Windows 10";
+		std::wstring s3 = L"Windows 11";
+
+		if (car_bomb.find(s2) != std::string::npos) {
+			car_bomb = s2;
+			return car_bomb;
+		}
+		else if (car_bomb.find(s3) != std::string::npos) {
+			car_bomb = s3;
+			return car_bomb;
+		}
+		else {
+			car_bomb = L"Unknown Windows Version";
+			return car_bomb;
+		}
+	}
+	catch (const std::exception&)
+	{
+		std::wstring error = L"Unknown";
+		return error;
+	}
+
+}
+
+std::wstring getwinver2() {
 
 	using namespace m4x1m1l14n;
 
@@ -40,14 +75,14 @@ std::wstring getwinver() {
 	}
 	catch (const std::exception&)
 	{
-		
-		return NULL; //change this you fucking retard
+		std::wstring error = L"Unknown";
+		return error;
 	}
 
-	return 0;
 }
 
 std::wstring getwinbuild() {
+
 	using namespace m4x1m1l14n;
 
 	try
@@ -60,11 +95,25 @@ std::wstring getwinbuild() {
 	}
 	catch (const std::exception&)
 	{
-
-		return NULL; //again
+		std::wstring error = L"Unknown";
+		return error;
 	}
 
-	return 0;
+	
+}
+
+void GetDesktopResolution(int& horizontal, int& vertical)
+{
+	RECT desktop;
+	// Get a handle to the desktop window
+	const HWND hDesktop = GetDesktopWindow();
+	// Get the size of screen to the variable desktop
+	GetWindowRect(hDesktop, &desktop);
+	// The top left corner will have coordinates (0,0)
+	// and the bottom right corner will have coordinates
+	// (horizontal, vertical)
+	horizontal = desktop.right;
+	vertical = desktop.bottom;
 }
 
 int main()
@@ -72,7 +121,8 @@ int main()
 	//debug
 	std::wcout << getusername() << std::endl;
 	std::wcout << gethostname() << std::endl;
-	std::wcout << getwinver() << std::endl;
+	std::wcout << getwinver1() << std::endl;
+	std::wcout << getwinver2() << std::endl;
 	std::wcout << getwinbuild() << std::endl;
 
 }
