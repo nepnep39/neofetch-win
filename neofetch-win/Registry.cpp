d29c7d9822215a576c0415e48246b888c8859bb7
nepnep <nepnep@hrzn.email>
2023-02-24 13:06:12 -0500

    regkey stuff

---
+++ neofetch-win/Registry.cpp
@@ -0,0 +1,13 @@
+#include <Registry.hpp>
+
+namespace m4x1m1l14n
+{
+	namespace Registry
+	{
+		RegistryKey_ptr ClassesRoot(new RegistryKey(HKEY_CLASSES_ROOT));
+		RegistryKey_ptr CurrentUser(new RegistryKey(HKEY_CURRENT_USER));
+		RegistryKey_ptr LocalMachine(new RegistryKey(HKEY_LOCAL_MACHINE));
+		RegistryKey_ptr Users(new RegistryKey(HKEY_USERS));
+		RegistryKey_ptr CurrentConfig(new RegistryKey(HKEY_CURRENT_CONFIG));
+	}
+}
--- neofetch-win/source.cpp
+++ neofetch-win/source.cpp
@@ -2,6 +2,7 @@
 #include <windows.h>
 #include <string.h>
 #include <Lmcons.h>
+#include <Registry.hpp>
 
 std::wstring getusername() {
 	
@@ -25,9 +26,53 @@ std::wstring gethostname() {
 	return userhost;
 }
 
+std::wstring getwinver() {
+
+	using namespace m4x1m1l14n;
+
+	try
+	{
+		auto key = Registry::LocalMachine->Open(L"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion");
+
+		auto logFileName = key->GetString(L"EditionId");
+
+		return logFileName;
+	}
+	catch (const std::exception&)
+	{
+		
+		return NULL; //change this you fucking retard
+	}
+
+	return 0;
+}
+
+std::wstring getwinbuild() {
+	using namespace m4x1m1l14n;
+
+	try
+	{
+		auto key = Registry::LocalMachine->Open(L"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion");
+
+		auto logFileName = key->GetString(L"DisplayVersion");
+
+		return logFileName;
+	}
+	catch (const std::exception&)
+	{
+
+		return NULL; //again
+	}
+
+	return 0;
+}
+
 int main()
 {
 	//debug
 	std::wcout << getusername() << std::endl;
 	std::wcout << gethostname() << std::endl;
+	std::wcout << getwinver() << std::endl;
+	std::wcout << getwinbuild() << std::endl;
+
 }
