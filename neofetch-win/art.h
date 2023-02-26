030163e692c29ead019d1e04cca48da995cd75fe
nepnep <nepnep@hrzn.email>
2023-02-26 16:24:33 -0500

    release

--- neofetch-win/art.h
+++ neofetch-win/art.h
@@ -1,64 +1,107 @@
 ﻿#pragma once
 
-// the reason that these constants are defined in a header is so that you can customize them!
+// the reason that these constants are defined in a header is so that you can easily customize them!
 
-const std::wstring win10art = LR"(
-                                ..,         
-                    ....,,:;+ccllll         
-      ...,,+:;  cllllllllllllllllll         
-,cclllllllllll  lllllllllllllllllll         
-llllllllllllll  lllllllllllllllllll         
-llllllllllllll  lllllllllllllllllll         
-llllllllllllll  lllllllllllllllllll         
-llllllllllllll  lllllllllllllllllll         
-llllllllllllll  lllllllllllllllllll         
-                                            
-llllllllllllll  lllllllllllllllllll         
-llllllllllllll  lllllllllllllllllll         
-llllllllllllll  lllllllllllllllllll         
-llllllllllllll  lllllllllllllllllll         
-llllllllllllll  lllllllllllllllllll         
-llllllllllllll  lllllllllllllllllll         
-`'ccllllllllll  lllllllllllllllllll         
-      `' \\*::  :ccllllllllllllllll         
-                       ````''*::cll         
-                                 ``         
-)";
+const std::wstring win10art01 = L"                                ..,";
+const std::wstring win10art02 = L"                    ....,,:;+ccllll";
+const std::wstring win10art03 = L"      ...,,+:;  cllllllllllllllllll";
+const std::wstring win10art04 = L",cclllllllllll  lllllllllllllllllll";
+const std::wstring win10art05 = L"llllllllllllll  lllllllllllllllllll";
+const std::wstring win10art06 = L"llllllllllllll  lllllllllllllllllll";
+const std::wstring win10art07 = L"llllllllllllll  lllllllllllllllllll";
+const std::wstring win10art08 = L"llllllllllllll  lllllllllllllllllll";
+const std::wstring win10art09 = L"llllllllllllll  lllllllllllllllllll";
+const std::wstring win10art10 = L"                                   ";
+const std::wstring win10art11 = L"llllllllllllll  lllllllllllllllllll";
+const std::wstring win10art12 = L"llllllllllllll  lllllllllllllllllll";
+const std::wstring win10art13 = L"llllllllllllll  lllllllllllllllllll";
+const std::wstring win10art14 = L"llllllllllllll  lllllllllllllllllll";
+const std::wstring win10art15 = L"llllllllllllll  lllllllllllllllllll";
+const std::wstring win10art16 = L"llllllllllllll  lllllllllllllllllll";
+const std::wstring win10art17 = L"`'ccllllllllll  lllllllllllllllllll";
+const std::wstring win10art18 = LR"(      `' \\*::  :ccllllllllllllllll)"; // <---- this line had to be a raw string literal because of the backslashes
+const std::wstring win10art19 = L"                       ````''*::cll";         // which the compiler assumes are escape chars (like \n), but if you're
+const std::wstring win10art20 = L"                                 ``";         // customizing the art feel free to change that
 
-const std::wstring win11art = LR"(
-lllllllllllllll   lllllllllllllll
-lllllllllllllll   lllllllllllllll
-lllllllllllllll   lllllllllllllll
-lllllllllllllll   lllllllllllllll
-lllllllllllllll   lllllllllllllll
-lllllllllllllll   lllllllllllllll
-lllllllllllllll   lllllllllllllll
-                                 
-lllllllllllllll   lllllllllllllll
-lllllllllllllll   lllllllllllllll
-lllllllllllllll   lllllllllllllll
-lllllllllllllll   lllllllllllllll
-lllllllllllllll   lllllllllllllll
-lllllllllllllll   lllllllllllllll
-lllllllllllllll   lllllllllllllll
-)";
+//                                ..,
+//                    ....,,:;+ccllll
+//      ...,,+:;  cllllllllllllllllll
+//,cclllllllllll  lllllllllllllllllll
+//llllllllllllll  lllllllllllllllllll
+//llllllllllllll  lllllllllllllllllll
+//llllllllllllll  lllllllllllllllllll
+//llllllllllllll  lllllllllllllllllll
+//llllllllllllll  lllllllllllllllllll
+//                                   
+//llllllllllllll  lllllllllllllllllll
+//llllllllllllll  lllllllllllllllllll
+//llllllllllllll  lllllllllllllllllll
+//llllllllllllll  lllllllllllllllllll
+//llllllllllllll  lllllllllllllllllll
+//llllllllllllll  lllllllllllllllllll
+//`'ccllllllllll  lllllllllllllllllll
+//      `' \\*::  :ccllllllllllllllll
+//                       ````''*::cll
+//                                 ``
+
+const std::wstring win11art01 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring win11art02 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring win11art03 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring win11art04 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring win11art05 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring win11art06 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring win11art07 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring win11art08 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring win11art09 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring win11art10 = L"                                   ";
+const std::wstring win11art11 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring win11art12 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring win11art13 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring win11art14 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring win11art15 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring win11art16 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring win11art17 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring win11art18 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring win11art19 = L"llllllllllllllll   llllllllllllllll";
+
+//llllllllllllllll   llllllllllllllll
+//llllllllllllllll   llllllllllllllll
+//llllllllllllllll   llllllllllllllll
+//llllllllllllllll   llllllllllllllll
+//llllllllllllllll   llllllllllllllll
+//llllllllllllllll   llllllllllllllll
+//llllllllllllllll   llllllllllllllll
+//llllllllllllllll   llllllllllllllll
+//llllllllllllllll   llllllllllllllll
+//
+//llllllllllllllll   llllllllllllllll
+//llllllllllllllll   llllllllllllllll
+//llllllllllllllll   llllllllllllllll
+//llllllllllllllll   llllllllllllllll
+//llllllllllllllll   llllllllllllllll
+//llllllllllllllll   llllllllllllllll
+//llllllllllllllll   llllllllllllllll
+//llllllllllllllll   llllllllllllllll
+//llllllllllllllll   llllllllllllllll
 
 // identical to win11art but you could customize this if you so choose
 
-const std::wstring unknownart = LR"(
-lllllllllllllll   lllllllllllllll
-lllllllllllllll   lllllllllllllll
-lllllllllllllll   lllllllllllllll
-lllllllllllllll   lllllllllllllll
-lllllllllllllll   lllllllllllllll
-lllllllllllllll   lllllllllllllll
-lllllllllllllll   lllllllllllllll
-                                 
-lllllllllllllll   lllllllllllllll
-lllllllllllllll   lllllllllllllll
-lllllllllllllll   lllllllllllllll
-lllllllllllllll   lllllllllllllll
-lllllllllllllll   lllllllllllllll
-lllllllllllllll   lllllllllllllll
-lllllllllllllll   lllllllllllllll
-)";
+const std::wstring unknownart01 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring unknownart02 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring unknownart03 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring unknownart04 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring unknownart05 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring unknownart06 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring unknownart07 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring unknownart08 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring unknownart09 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring unknownart10 = L"                                   ";
+const std::wstring unknownart11 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring unknownart12 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring unknownart13 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring unknownart14 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring unknownart15 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring unknownart16 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring unknownart17 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring unknownart18 = L"llllllllllllllll   llllllllllllllll";
+const std::wstring unknownart19 = L"llllllllllllllll   llllllllllllllll";
--- neofetch-win/source.cpp
+++ neofetch-win/source.cpp
@@ -7,6 +7,13 @@
 #include <art.h>
 #include <Registry.hpp>
 
+#define setlght SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 3)
+#define setdark SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9)
+#define setdflt SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7)
+
+// win10 ascii art terminal colour should be 3, win11 colour should be 9, default (for text) is 7
+// you can modify these defines to customize the output colours!
+
 std::wstring getusername() {
 	
 	TCHAR username[UNLEN + 1]{};
@@ -163,34 +170,21 @@ int artsel() {
 
 	std::wstring win10 = L"Windows 10";
 	std::wstring win11 = L"Windows 11";
+
 	int artint;
-	int c;
-
-	HANDLE terminal = GetStdHandle(STD_OUTPUT_HANDLE);
-
-	// win10 terminal colour should be 3, win11 colour should be 9
 
 	if (getwinver().find(win10) != std::string::npos) {
 
-		c = 3;
-
-		SetConsoleTextAttribute(terminal, c);
 		return artint = 0;
 
 	}
 	else if (getwinver().find(win11) != std::string::npos) {
 
-		c = 9;
-
-		SetConsoleTextAttribute(terminal, c);
 		return artint = 1;
 
 	}
 	else {
 		
-		c = 3;
-
-		SetConsoleTextAttribute(terminal, c);
 		return artint = 2;
 
 	}
@@ -278,24 +272,6 @@ int debug() {
 
 	std::cout << getmem(2) << " MB / " << getmem(1) << " MB (" << getmem(0) << "% in use)";
 
-	if (artsel() == 0) {
-
-		std::wcout << win10art << std::endl;
-	}
-	else if (artsel() == 1) {
-
-		std::wcout << win11art << std::endl;
-	}
-	else if (artsel() == 2) {
-
-		std::wcout << unknownart << std::endl;
-	}
-	else {	
-
-		return 0;
-	}
-
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
 	return 0;
 
 }
@@ -303,26 +279,123 @@ int debug() {
 void neofetch() {
 
 	// actual render
+	std::wstring divider = L"--------------";
+
+	int uptime = std::chrono::duration_cast<std::chrono::milliseconds>(getuptime()).count();
+
+	int minutes = (int)((uptime / (1000 * 60)) % 60);
+	int hours = (int)((uptime / (1000 * 60 * 60)) % 24);
+
+	std::string plural1;
+	std::string plural2;
+
+	if (hours == 1)
+	{
+		plural1 = "hour";
+	}
+	else {
+		plural1 = "hours";
+	}
+
+	if (minutes == 1)
+	{
+		plural2 = "minute";
+	}
+	else {
+		plural2 = "minutes";
+	}
+
+	int horizontal = 0;
+	int vertical = 0;
+	getresolution(horizontal, vertical);
 
 	if (artsel() == 0) {
 
-		std::wcout << std::left << win10art;
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
-		std::wcout << std::setw(40) << std::right << getcpu() << std::endl;
+		using namespace std;
+
+		setlght; wcout << win10art01 << setw(19) << endl;
+		setlght; wcout << win10art02 << setw(15) << right << getusername(); setdflt; wcout << '@'; setlght;  wcout << gethostname() << endl; setdflt;
+		setlght; wcout << win10art03; setdflt; wcout << setw(26) << right << divider << endl;
+		setlght; wcout << win10art04 << setw(16) << right << L"OS: "; setdflt; wcout << getwinver() << endl;
+		setlght; wcout << win10art05 << setw(19) << right << L"Build: "; setdflt; wcout << getwinbuild() << endl;
+		setlght; wcout << win10art06 << setw(20) << right << L"Uptime: "; setdflt; cout << hours << " " << plural1 << ", " << minutes << " " << plural2 << std::endl;
+		setlght; wcout << win10art07 << setw(24) << right << L"Resolution: "; setdflt; cout << horizontal << 'x' << vertical << std::endl;
+		setlght; wcout << win10art08 << setw(22) << right << L"Terminal: "; setdflt; wcout << getconsole() << endl;
+		setlght; wcout << win10art09 << setw(17) << right << L"CPU: "; setdflt; wcout << getcpu() << endl;
+		setlght; wcout << win10art10 << setw(17) << right << L"GPU: "; setdflt; wcout << getgpu() << endl;
+		setlght; wcout << win10art11 << setw(20) << right << L"Memory: "; setdflt; cout << getmem(2) << " MB / " << getmem(1) << " MB (" << getmem(0) << "% in use)" << endl;
+		setlght; wcout << win10art12 << setw(19) << endl;
+		setlght; wcout << win10art13 << setw(19) << endl;
+		setlght; wcout << win10art14 << setw(19) << endl;
+		setlght; wcout << win10art15 << setw(19) << endl;
+		setlght; wcout << win10art16 << setw(19) << endl;
+		setlght; wcout << win10art17 << setw(19) << endl;
+		setlght; wcout << win10art18 << setw(19) << endl;
+		setlght; wcout << win10art19 << setw(19) << endl;
+		setlght; wcout << win10art20 << setw(19) << endl;
+
+		setdflt;
+
 	}
 	else if (artsel() == 1) {
 
-		std::wcout << std::left << win11art << std::setw(40) << std::right << "Sample text" << std::endl;
+		using namespace std;
+
+		setdark; wcout << win11art01 << setw(15) << right << getusername(); setdflt; wcout << '@'; setdark;  wcout << gethostname() << endl; setdflt;
+		setdark; wcout << win11art02; setdflt; wcout << setw(26) << right << divider << endl;
+		setdark; wcout << win11art03 << setw(16) << right << L"OS: "; setdflt; wcout << getwinver() << endl;
+		setdark; wcout << win11art04 << setw(19) << right << L"Build: "; setdflt; wcout << getwinbuild() << endl;
+		setdark; wcout << win11art05 << setw(20) << right << L"Uptime: "; setdflt; cout << hours << " " << plural1 << ", " << minutes << " " << plural2 << std::endl;
+		setdark; wcout << win11art06 << setw(24) << right << L"Resolution: "; setdflt; cout << horizontal << 'x' << vertical << std::endl;
+		setdark; wcout << win11art07 << setw(22) << right << L"Terminal: "; setdflt; wcout << getconsole() << endl;
+		setdark; wcout << win11art08 << setw(17) << right << L"CPU: "; setdflt; wcout << getcpu() << endl;
+		setdark; wcout << win11art09 << setw(17) << right << L"GPU: "; setdflt; wcout << getgpu() << endl;
+		setdark; wcout << win11art10 << setw(20) << right << L"Memory: "; setdflt; cout << getmem(2) << " MB / " << getmem(1) << " MB (" << getmem(0) << "% in use)" << endl;
+		setdark; wcout << win11art11 << setw(19) << endl;
+		setdark; wcout << win11art12 << setw(19) << endl;
+		setdark; wcout << win11art13 << setw(19) << endl;
+		setdark; wcout << win11art14 << setw(19) << endl;
+		setdark; wcout << win11art15 << setw(19) << endl;
+		setdark; wcout << win11art16 << setw(19) << endl;
+		setdark; wcout << win11art17 << setw(19) << endl;
+		setdark; wcout << win11art18 << setw(19) << endl;
+		setdark; wcout << win11art19 << setw(19) << endl;
+
+		setdflt;
+	
 	}
 	else if (artsel() == 2) {
 
-		std::wcout << std::left << unknownart << std::setw(40) << std::right << "Sample text" << std::endl;
+		using namespace std;
+
+		setlght; wcout << unknownart01 << setw(15) << right << getusername(); setdflt; wcout << '@'; setlght;  wcout << gethostname() << endl; setdflt;
+		setlght; wcout << unknownart02; setdflt; wcout << setw(26) << right << divider << endl;
+		setlght; wcout << unknownart03 << setw(16) << right << L"OS: "; setdflt; wcout << getwinver() << endl;
+		setlght; wcout << unknownart04 << setw(19) << right << L"Build: "; setdflt; wcout << getwinbuild() << endl;
+		setlght; wcout << unknownart05 << setw(20) << right << L"Uptime: "; setdflt; cout << hours << " " << plural1 << ", " << minutes << " " << plural2 << std::endl;
+		setlght; wcout << unknownart06 << setw(24) << right << L"Resolution: "; setdflt; cout << horizontal << 'x' << vertical << std::endl;
+		setlght; wcout << unknownart07 << setw(22) << right << L"Terminal: "; setdflt; wcout << getconsole() << endl;
+		setlght; wcout << unknownart08 << setw(17) << right << L"CPU: "; setdflt; wcout << getcpu() << endl;
+		setlght; wcout << unknownart09 << setw(17) << right << L"GPU: "; setdflt; wcout << getgpu() << endl;
+		setlght; wcout << unknownart10 << setw(20) << right << L"Memory: "; setdflt; cout << getmem(2) << " MB / " << getmem(1) << " MB (" << getmem(0) << "% in use)" << endl;
+		setlght; wcout << unknownart11 << setw(19) << endl;
+		setlght; wcout << unknownart12 << setw(19) << endl;
+		setlght; wcout << unknownart13 << setw(19) << endl;
+		setlght; wcout << unknownart14 << setw(19) << endl;
+		setlght; wcout << unknownart15 << setw(19) << endl;
+		setlght; wcout << unknownart16 << setw(19) << endl;
+		setlght; wcout << unknownart17 << setw(19) << endl;
+		setlght; wcout << unknownart18 << setw(19) << endl;
+		setlght; wcout << unknownart19 << setw(19) << endl;
+
+		setdflt;
+		
 	}
 	else {
 		
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
 		std::cout << "Unable to determine Windows version." << std::endl;
-		std::system("PAUSE");
+		system("PAUSE");
 
 	}
 }
