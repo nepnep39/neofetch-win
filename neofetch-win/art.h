b397b036638d30d22fe973dd863dbefc9387e713
nepnep <nepnep@hrzn.email>
2023-02-26 11:52:07 -0500

    for real this time

--- neofetch-win/art.h
+++ neofetch-win/art.h
@@ -1,60 +1,60 @@
 ﻿#include <string>
 
 std::string win10art = R"(
-                                        ..,
-                            ....,,:;+ccllll
-              ...,,+:;  cllllllllllllllllll
-        ,cclllllllllll  lllllllllllllllllll
-        llllllllllllll  lllllllllllllllllll
-        llllllllllllll  lllllllllllllllllll
-        llllllllllllll  lllllllllllllllllll
-        llllllllllllll  lllllllllllllllllll
-        llllllllllllll  lllllllllllllllllll
+                                ..,
+                    ....,,:;+ccllll
+      ...,,+:;  cllllllllllllllllll
+,cclllllllllll  lllllllllllllllllll
+llllllllllllll  lllllllllllllllllll
+llllllllllllll  lllllllllllllllllll
+llllllllllllll  lllllllllllllllllll
+llllllllllllll  lllllllllllllllllll
+llllllllllllll  lllllllllllllllllll
                                            
-        llllllllllllll  lllllllllllllllllll
-        llllllllllllll  lllllllllllllllllll
-        llllllllllllll  lllllllllllllllllll
-        llllllllllllll  lllllllllllllllllll
-        llllllllllllll  lllllllllllllllllll
-        llllllllllllll  lllllllllllllllllll
-        `'ccllllllllll  lllllllllllllllllll
-              `' \\*::  :ccllllllllllllllll
-                               ````''*::cll
-                                         ``
-        )";
+llllllllllllll  lllllllllllllllllll
+llllllllllllll  lllllllllllllllllll
+llllllllllllll  lllllllllllllllllll
+llllllllllllll  lllllllllllllllllll
+llllllllllllll  lllllllllllllllllll
+llllllllllllll  lllllllllllllllllll
+`'ccllllllllll  lllllllllllllllllll
+      `' \\*::  :ccllllllllllllllll
+                       ````''*::cll
+                                 ``
+)";
 
 std::string win11art = R"(
-        ▟███████████████  ███████████████▙
-        ████████████████  ████████████████
-        ████████████████  ████████████████
-        ████████████████  ████████████████
-        ████████████████  ████████████████
-        ████████████████  ████████████████
-        ████████████████  ████████████████
-                                          
-        ████████████████  ████████████████
-        ████████████████  ████████████████
-        ████████████████  ████████████████
-        ████████████████  ████████████████
-        ████████████████  ████████████████
-        ████████████████  ████████████████
-        ▜███████████████  ███████████████▛
-        )";
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+                                 
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+)";
 
 std::string unknownart = R"(
-        ▟███████████████  ███████████████▙
-        ████████████████  ████████████████
-        ████████████████  ████████████████
-        ████████████████  ████████████████
-        ████████████████  ████████████████
-        ████████████████  ████████████████
-        ████████████████  ████████████████
-                                          
-        ████████████████  ████████████████
-        ████████████████  ████████████████
-        ████████████████  ████████████████
-        ████████████████  ████████████████
-        ████████████████  ████████████████
-        ████████████████  ████████████████
-        ▜███████████████  ███████████████▛
-        )";
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+                                 
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+lllllllllllllll   lllllllllllllll
+)";
