471a4b79f6276d1aec84094550f11519f73d1d30
nepnep <nepnep@hrzn.email>
2023-09-07 13:04:28 -0400

    v1.2.0 source

--- neofetch-win/source.cpp
+++ neofetch-win/source.cpp
@@ -9,6 +9,7 @@
 
 #define setlght SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 3)
 #define setdflt SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7)
+#define setbarc SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 13)
 
 // win10 ascii art terminal colour should be 3, default (for text) is 7
 // you can modify these defines to customize the output colours!
@@ -114,15 +115,40 @@ std::wstring getwinbuild() {
 	
 }
 
-void getresolution(int& horizontal, int& vertical)
-{
-	RECT desktop;
-	const HWND hDesktop = GetDesktopWindow();
+std::wstring getwinbuildnum() {
 
-	GetWindowRect(hDesktop, &desktop);
+	using namespace m4x1m1l14n;
+
+	try {
+		auto key = Registry::LocalMachine->Open(L"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion");
+
+		auto buildnum = key->GetString(L"CurrentBuildNumber");
+
+		return buildnum;
+	}
+	catch (const std::exception&)
+	{
+		std::wstring error = L"Unknown build number";
+		return error;
+	}
+
+}
+
+void getresolution(int& horizontal, int& vertical, int& hz)
+{
+	DISPLAY_DEVICE dd{};
+	dd.cb = sizeof(dd);
 	
-	horizontal = desktop.right;
-	vertical = desktop.bottom;
+	EnumDisplayDevices(NULL, 0, &dd, 0);
+	DEVMODE dm{};
+	dm.dmSize = sizeof(dm);
+	dm.dmDriverExtra = 0;
+	
+	EnumDisplaySettings(dd.DeviceName, ENUM_CURRENT_SETTINGS, &dm);
+	
+	horizontal = dm.dmPelsWidth;
+	vertical = dm.dmPelsHeight;
+	hz = dm.dmDisplayFrequency;
 }
 
 std::wstring getconsole() {
@@ -231,6 +257,133 @@ int getmem(int typesel) {
 	}
 }
 
+void getdisk()
+{
+	ULARGE_INTEGER FreeBytesAvailableToCaller;
+	ULARGE_INTEGER TotalNumberOfBytes;
+	ULARGE_INTEGER TotalNumberOfFreeBytes;
+
+	double total{};
+	double free{};
+
+	BOOL check = GetDiskFreeSpaceExA("C:\\", &FreeBytesAvailableToCaller, &TotalNumberOfBytes, &TotalNumberOfFreeBytes);
+
+	if (check)
+	{
+	/*	printf("GB free: %llu\n", TotalNumberOfFreeBytes.QuadPart / (1024 * 1024));
+		printf("GB total: %llu\n", TotalNumberOfBytes.QuadPart / (1024 * 1024));*/
+
+		ULONGLONG totalspace = TotalNumberOfBytes.QuadPart / 1024 / 1024;
+		if (totalspace <= INT_MAX) total = static_cast<double>(totalspace);
+
+		ULONGLONG freespace = TotalNumberOfFreeBytes.QuadPart / 1024 / 1024;
+		if (freespace <= INT_MAX) free = static_cast<double>(freespace);
+
+		double totalgb = total / 1000;
+		double freegb = free / 1000;
+
+		totalgb = std::ceil(totalgb * 100.0) / 100.0;
+		freegb = std::ceil(freegb * 100.0) / 100.0;
+
+
+		std::wcout << LR"(C:\ )" << totalgb << L" GB (" << freegb << L" GB free)";
+
+	}
+	else
+	{
+		std::wcout << L"Disk info unknown";
+	
+	}
+
+}
+
+void drawbar(double input)
+{
+	//bar colour define is at top of file
+
+	std::cout << "-=[ ";
+
+	int input1 = input / 5;
+
+	setbarc;
+
+	int i = 0;
+
+	while (i < input1)
+	{
+		std::cout << "/";
+		i++;
+	}
+	
+	setdflt;
+
+	while (i < 20)
+	{
+		std::cout << "/";
+		i++;
+	}
+
+	std::cout << " ]=-";
+}
+
+void membar()
+{
+	MEMORYSTATUSEX mem{};
+
+	mem.dwLength = sizeof(mem);
+
+	GlobalMemoryStatusEx(&mem);
+
+	double percent = mem.dwMemoryLoad;
+	int total = mem.ullTotalPhys / 1048576;
+	int avail = mem.ullAvailPhys / 1048576;
+
+	//std::wcout << percent;
+
+	drawbar(percent);
+
+}
+
+void diskbar()
+{
+	ULARGE_INTEGER FreeBytesAvailableToCaller;
+	ULARGE_INTEGER TotalNumberOfBytes;
+	ULARGE_INTEGER TotalNumberOfFreeBytes;
+
+	double total{};
+	double free{};
+
+	BOOL check = GetDiskFreeSpaceExA("C:\\", &FreeBytesAvailableToCaller, &TotalNumberOfBytes, &TotalNumberOfFreeBytes);
+
+	if (check)
+	{
+
+		ULONGLONG totalspace = TotalNumberOfBytes.QuadPart / 1024 / 1024;
+		if (totalspace <= INT_MAX) total = static_cast<double>(totalspace);
+
+		ULONGLONG freespace = TotalNumberOfFreeBytes.QuadPart / 1024 / 1024;
+		if (freespace <= INT_MAX) free = static_cast<double>(freespace);
+
+		double totalgb = total / 1000;
+		double freegb = free / 1000;
+
+		double percent = free / total;
+		percent = 1.0 - percent;
+		percent = percent * 100;
+
+		//std::wcout << percent;
+
+		drawbar(percent);
+
+	}
+	else
+	{
+		std::wcout << L"Unable to draw disk bar";
+
+	}
+
+}
+
 void colourbar() {
 
 	// std::setlocale(LC_ALL, "en_US.UTF-16");
@@ -252,7 +405,7 @@ void colourbar() {
 
 int debug() {
 
-	// calling this function lists all vars in a list without labels or any fancy stuff
+	// calling this function lists (almost) all vars in a list without labels or any fancy stuff
 
 	std::wcout << getusername() << std::endl;
 	std::wcout << gethostname() << std::endl;
@@ -299,7 +452,9 @@ int debug() {
 	
 	int horizontal = 0;
 	int vertical = 0;
-	getresolution(horizontal, vertical);
+	int hz = 0;
+
+	getresolution(horizontal, vertical, hz);
 	std::cout << horizontal << 'x' << vertical << std::endl;
 
 	std::wcout << getconsole() << std::endl;
@@ -340,15 +495,18 @@ void neofetch() {
 
 	std::wstring divider = L"--------------";
 
-	int uptime = std::chrono::duration_cast<std::chrono::milliseconds>(getuptime()).count();
+	int uptime = std::chrono::duration_cast<std::chrono::seconds>(getuptime()).count();
 
-	int minutes = (int)((uptime / (1000 * 60)) % 60);
-	int hours = (int)((uptime / (1000 * 60 * 60)) % 24);
-	int days = (int)(uptime / (1000 * 60 * 60 * 24));
+	int minutes = (int)((uptime / (60)) % 60);
+	int hours = (int)((uptime / (60 * 60)) % 24);
+	int days = (int)(uptime / (60 * 60 * 24));
+	//int months = (long)(uptime / (1000 * 60 * 60 * 24 * 30) % 12);
+	// this WILL overflow
 
 	std::string plural1;
 	std::string plural2;
 	std::string plural3;
+	//std::string plural4;
 
 	if (hours == 1)
 	{
@@ -373,10 +531,19 @@ void neofetch() {
 	else {
 		plural3 = "days";
 	}
+	/*if (months == 1)
+	{
+		plural4 = "month";
+	}
+	else {
+		plural4 = "months";
+	}*/
 
 	int horizontal = 0;
 	int vertical = 0;
-	getresolution(horizontal, vertical);
+	int hz = 0;
+
+	getresolution(horizontal, vertical, hz);
 
 	if (artsel() == 0) {
 
@@ -386,21 +553,21 @@ void neofetch() {
 		setlght; wcout << win10art02 << setw(12 + username.length()) << right << getusername(); setdflt; cout << '@'; setlght;  wcout << gethostname() << endl; setdflt;
 		setlght; wcout << win10art03; setdflt; wcout << setw(26) << right << divider << endl;
 		setlght; wcout << win10art04 << setw(16) << right << L"OS: "; setdflt; wcout << getwinver() << endl;
-		setlght; wcout << win10art05 << setw(19) << right << L"Build: "; setdflt; wcout << getwinbuild() << endl;
+		setlght; wcout << win10art05 << setw(19) << right << L"Build: "; setdflt; wcout << getwinbuild() << " (" << getwinbuildnum() << ')' << endl;
 		setlght; wcout << win10art06 << setw(20) << right << L"Uptime: "; setdflt; cout << days << " " << plural3 << ", " << hours << " " << plural1 << ", " << minutes << " " << plural2 << std::endl;
-		setlght; wcout << win10art07 << setw(24) << right << L"Resolution: "; setdflt; cout << horizontal << 'x' << vertical << std::endl;
+		setlght; wcout << win10art07 << setw(24) << right << L"Resolution: "; setdflt; cout << horizontal << 'x' << vertical << " @" << hz << "Hz" << std::endl;
 		setlght; wcout << win10art08 << setw(22) << right << L"Terminal: "; setdflt; wcout << getconsole() << endl;
 		setlght; wcout << win10art09 << setw(17) << right << L"CPU: "; setdflt; wcout << getcpu() << endl;
 		setlght; wcout << win10art10 << setw(17) << right << L"GPU: "; setdflt; wcout << getgpu() << endl;
 		setlght; wcout << win10art11 << setw(20) << right << L"Memory: "; setdflt; cout << getmem(2) << " MB / " << getmem(1) << " MB (" << getmem(0) << "% in use)" << endl;
-		setlght; wcout << win10art12 << setw(19) << endl;
-		setlght; wcout << win10art09 << setw(13) << right;  colourbar(); cout << endl;
-		setlght; wcout << win10art14 << setw(19) << endl;
+		setlght; wcout << win10art12 << setw(18) << right << L"Disk: "; setdflt; getdisk(); std::cout << endl;
+		setlght; wcout << win10art13 << setw(17) << endl;
+		setlght; wcout << win10art14 << setw(19) << right << L"Mem%:  "; setdflt; membar(); std::cout << endl;
 		setlght; wcout << win10art15 << setw(19) << endl;
-		setlght; wcout << win10art16 << setw(19) << endl;
+		setlght; wcout << win10art16 << setw(19) << right << L"Disk%: "; setdflt; diskbar(); std::cout << endl;
 		setlght; wcout << win10art17 << setw(19) << endl;
 		setlght; wcout << win10art18 << setw(19) << endl;
-		setlght; wcout << win10art19 << setw(19) << endl;
+		setlght; wcout << win10art19 << setw(13) << right; colourbar(); cout << endl;
 		setlght; wcout << win10art20 << setw(19) << endl;
 
 		setdflt;
@@ -413,21 +580,21 @@ void neofetch() {
 		setlght; wcout << win11art01 << setw(12 + username.length()) << right << getusername(); setdflt; cout << '@'; setlght;  wcout << gethostname() << endl; setdflt;
 		setlght; wcout << win11art02; setdflt; wcout << setw(26) << right << divider << endl;
 		setlght; wcout << win11art03 << setw(16) << right << L"OS: "; setdflt; wcout << getwinver() << endl;
-		setlght; wcout << win11art04 << setw(19) << right << L"Build: "; setdflt; wcout << getwinbuild() << endl;
+		setlght; wcout << win11art04 << setw(19) << right << L"Build: "; setdflt; wcout << getwinbuild() << " (" << getwinbuildnum() << ')' << endl;
 		setlght; wcout << win11art05 << setw(20) << right << L"Uptime: "; setdflt; cout << days << " " << plural3 << ", " << hours << " " << plural1 << ", " << minutes << " " << plural2 << std::endl;
-		setlght; wcout << win11art06 << setw(24) << right << L"Resolution: "; setdflt; cout << horizontal << 'x' << vertical << std::endl;
+		setlght; wcout << win11art06 << setw(24) << right << L"Resolution: "; setdflt; cout << horizontal << 'x' << vertical << " @" << hz << "Hz" << std::endl;
 		setlght; wcout << win11art07 << setw(22) << right << L"Terminal: "; setdflt; wcout << getconsole() << endl;
 		setlght; wcout << win11art08 << setw(17) << right << L"CPU: "; setdflt; wcout << getcpu() << endl;
 		setlght; wcout << win11art09 << setw(17) << right << L"GPU: "; setdflt; wcout << getgpu() << endl;
 		setlght; wcout << win11art10 << setw(20) << right << L"Memory: "; setdflt; cout << getmem(2) << " MB / " << getmem(1) << " MB (" << getmem(0) << "% in use)" << endl;
-		setlght; wcout << win11art11 << setw(19) << endl;
-		setlght; wcout << win11art12 << setw(13) << right;  colourbar(); cout << endl;
-		setlght; wcout << win11art13 << setw(19) << endl;
+		setlght; wcout << win11art11 << setw(18) << right << L"Disk: "; setdflt; getdisk(); std::cout << endl;
+		setlght; wcout << win11art12 << setw(19) << endl;
+		setlght; wcout << win11art13 << setw(19) << right << L"Mem%:  "; setdflt; membar(); std::cout << endl;
 		setlght; wcout << win11art14 << setw(19) << endl;
-		setlght; wcout << win11art15 << setw(19) << endl;
+		setlght; wcout << win11art15 << setw(19) << right << L"Disk%: "; setdflt; diskbar(); std::cout << endl;
 		setlght; wcout << win11art16 << setw(19) << endl;
 		setlght; wcout << win11art17 << setw(19) << endl;
-		setlght; wcout << win11art18 << setw(19) << endl;
+		setlght; wcout << win11art18 << setw(13) << right; colourbar(); cout << endl;
 		setlght; wcout << win11art19 << setw(19) << endl;
 
 		setdflt;
@@ -440,21 +607,21 @@ void neofetch() {
 		setlght; wcout << unknownart01 << setw(12 + username.length()) << right << getusername(); setdflt; cout << '@'; setlght;  wcout << gethostname() << endl; setdflt;
 		setlght; wcout << unknownart02; setdflt; wcout << setw(26) << right << divider << endl;
 		setlght; wcout << unknownart03 << setw(16) << right << L"OS: "; setdflt; wcout << getwinver() << endl;
-		setlght; wcout << unknownart04 << setw(19) << right << L"Build: "; setdflt; wcout << getwinbuild() << endl;
+		setlght; wcout << unknownart04 << setw(19) << right << L"Build: "; setdflt; wcout << getwinbuild() << " (" << getwinbuildnum() << ')' << endl;
 		setlght; wcout << unknownart05 << setw(20) << right << L"Uptime: "; setdflt; cout << days << " " << plural3 << ", " << hours << " " << plural1 << ", " << minutes << " " << plural2 << std::endl;
-		setlght; wcout << unknownart06 << setw(24) << right << L"Resolution: "; setdflt; cout << horizontal << 'x' << vertical << std::endl;
+		setlght; wcout << unknownart06 << setw(24) << right << L"Resolution: "; setdflt; cout << horizontal << 'x' << vertical << " @" << hz << "Hz" << std::endl;
 		setlght; wcout << unknownart07 << setw(22) << right << L"Terminal: "; setdflt; wcout << getconsole() << endl;
 		setlght; wcout << unknownart08 << setw(17) << right << L"CPU: "; setdflt; wcout << getcpu() << endl;
 		setlght; wcout << unknownart09 << setw(17) << right << L"GPU: "; setdflt; wcout << getgpu() << endl;
 		setlght; wcout << unknownart10 << setw(20) << right << L"Memory: "; setdflt; cout << getmem(2) << " MB / " << getmem(1) << " MB (" << getmem(0) << "% in use)" << endl;
-		setlght; wcout << unknownart11 << setw(19) << endl;
-		setlght; wcout << unknownart12 << setw(13) << right;  colourbar(); cout << endl;
-		setlght; wcout << unknownart13 << setw(19) << endl;
+		setlght; wcout << unknownart11 << setw(18) << right << L"Disk: "; setdflt; getdisk(); std::cout << endl;
+		setlght; wcout << unknownart12 << setw(19) << endl;
+		setlght; wcout << unknownart13 << setw(19) << right << L"Mem%:  "; setdflt; membar(); std::cout << endl;
 		setlght; wcout << unknownart14 << setw(19) << endl;
-		setlght; wcout << unknownart15 << setw(19) << endl;
+		setlght; wcout << unknownart15 << setw(19) << right << L"Disk%: "; setdflt; diskbar(); std::cout << endl;
 		setlght; wcout << unknownart16 << setw(19) << endl;
 		setlght; wcout << unknownart17 << setw(19) << endl;
-		setlght; wcout << unknownart18 << setw(19) << endl;
+		setlght; wcout << unknownart18 << setw(13) << right; colourbar(); cout << endl;
 		setlght; wcout << unknownart19 << setw(19) << endl;
 
 		setdflt;
