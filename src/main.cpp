2f05327d90c290c4ee24094d8c823f2ab99a5caa
Izumi <99078094+WhereIsF1@users.noreply.github.com>
2025-07-21 14:52:23 +0200

    Remove debug mode and related files from project
    Eliminated debug mode support from main.cpp and removed references to debug.cpp and debug.h from the project files. This simplifies the build and execution by always running neofetch, and cleans up unused debug code and configuration.
    
    woopsie (fumo fumo)

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,14 @@
 #include <debug.h>
 
 void neofetch();
-int debug();
 
 int main(int argc, char* argv[]) {
 
     SetConsoleOutputCP(CP_UTF8);
     std::setlocale(LC_ALL, "en_US.UTF-8");
 
-
-    bool debugMode = false;
-
     for (int i = 1; i < argc; ++i) {
         std::string arg = argv[i];
-        if (arg == "-d" || arg == "--debug") {
-            //debugMode = true;
-        }
         if (arg == "-h" || arg == "--help") {
             fprintf(stdout, "Usage: neofetch-win [-h|--help]\n");
             fprintf(stdout, "Github: https://github.com/nepnep39/neofetch-win\n");
@@ -27,12 +20,7 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    if (debugMode) {
-        debug();
-    }
-    else {
-        neofetch();
-    }
+    neofetch();
 
     return 0;
 }
